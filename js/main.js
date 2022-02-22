3297e93dfc4849011a7a0681241ba42b45a3306e
Volodymyr <volodymyr.sekh.dev@gmail.com>
2022-02-22 13:19:34 +0200

    add updates: post message on page

--- js/main.js
+++ js/main.js
@@ -6,3 +6,16 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+
+  
+  document.getElementById("myBtn").addEventListener("click", function() {
+  let textEl = document.getElementById("my-txt");
+  let d = document.getElementById("fan-msg");
+      d.innerHTML = `${textEl.value}`;  
+      textEl.value = "";
+  let dataEl = document.getElementById("msg-data");
+  let today = new Date();
+  let msgDate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  dataEl.innerHTML = msgDate;
+  });
